112625533bc3fc2a016ac199e413f13f827e865c
odnath <od.nathmnair1@gmail.com>
2025-05-13 10:14:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// Get the current URL
+var currentURL = location.href;
+
+// Get the length of the URL
+var urlLength = currentURL.length;
+
+// Display the result
+alert("The length of the URL is: " + urlLength);
